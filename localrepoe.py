c32e190434982139501798585f56a593bd7b345d
subhansu <sarangisubhansu@gmail.com>
2024-11-15 16:30:16 +0530

    adding changes made for stash

--- localrepoe.py
+++ localrepoe.py
@@ -3,3 +3,5 @@ print ("Hello , Trying to add new file from Local repo")
 print ("adding change by 2nd developer")
 
 print ("adding change by 3rd developer")
+
+print ("for stashing addig thus line")
